8ebfe01760d143260cbd7442433b4681a90c7e73
Maria Jose <mariaajose17@gmail.com>
2023-09-27 17:23:36 -0500

    feat: new calculator

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import { useState } from "react";
+import "./App.css";
+
+const CALCULATOR_KEYS = [
+  {
+    nameKey: "7",
+    className: "number",
+    id: "seven",
+  },
+  {
+    nameKey: "8",
+    className: "number",
+    id: "eight",
+  },
+  {
+    nameKey: "9",
+    className: "number",
+    id: "nine",
+  },
+  {
+    nameKey: "4",
+    className: "number",
+    id: "four",
+  },
+  {
+    nameKey: "5",
+    className: "number",
+    id: "five",
+  },
+  {
+    nameKey: "6",
+    className: "number",
+    id: "six",
+  },
+  {
+    nameKey: "1",
+    className: "number",
+    id: "one",
+  },
+  {
+    nameKey: "2",
+    className: "number",
+    id: "two",
+  },
+  {
+    nameKey: "3",
+    className: "number",
+    id: "three",
+  },
+  {
+    nameKey: "0",
+    className: "number",
+    id: "zero",
+  },
+  {
+    nameKey: "=",
+    className: "equals",
+    id: "equals",
+  },
+  {
+    nameKey: "+",
+    className: "math-operation",
+    id: "add",
+  },
+  {
+    nameKey: "-",
+    className: "math-operation",
+    id: "subtract",
+  },
+  {
+    nameKey: "x",
+    className: "math-operation",
+    id: "multiply",
+  },
+  {
+    nameKey: "/",
+    className: "math-operation",
+    id: "divide",
+  },
+  {
+    nameKey: ".",
+    className: "math-number",
+    id: "decimal",
+  },
+  {
+    nameKey: "AC",
+    className: "ac",
+    id: "clear",
+  },
+];
+
+function App() {
+  const [result, setResult] = useState(0);
+
+  const [operation, setOperation] = useState("");
+
+  const handleEqual = () => {
+    setResult(eval(operation));
+  };
+
+  const handleClear = () => {
+    setOperation("");
+    setResult(0);
+  };
+
+  const handleKey = (key) => {
+    const specialKeys = {
+      AC: () => handleClear(),
+      "=": () => handleEqual(),
+      x: () => handleKey("*"),
+    };
+    if (specialKeys[key]) return specialKeys[key]();
+
+    return setOperation((op) => op.concat(key));
+  };
+
+  return (
+    <>
+      <div id="calculator">
+        <div id="display">
+          <div id="operation">
+            {operation.replace(/\*/g, "x")}
+            {result > 0 && `=${result}`}
+          </div>
+          <div id="result">{result}</div>
+        </div>
+        <div className="container">
+          {CALCULATOR_KEYS.map(({ nameKey, className, id }) => {
+            return (
+              <div
+                onClick={() => handleKey(nameKey)}
+                className={className}
+                id={id}
+                key={id}
+              >
+                {nameKey}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
